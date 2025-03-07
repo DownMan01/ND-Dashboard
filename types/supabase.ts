0ada5b925399c8d0ddfe133662518c85539bcaaa
J <98541904+DownMan01@users.noreply.github.com>
2025-03-07 13:34:33 +0800

    Update supabase.ts

--- types/supabase.ts
+++ types/supabase.ts
@@ -31,7 +31,7 @@ export interface Database {
           backers?: string[]
           chain: string
           cost?: number | null
-          stage?: "active" | "upcoming" | "ended"
+          stage?: "Active" | "Upcoming" | "Ended"
           requirements?: Json
           how_to_steps?: Json
           user_id: string
@@ -47,7 +47,7 @@ export interface Database {
           backers?: string[]
           chain?: string
           cost?: number | null
-          stage?: "active" | "upcoming" | "ended"
+          stage?: "Active" | "Upcoming" | "Ended"
           requirements?: Json
           how_to_steps?: Json
           user_id?: string
